a9d2264fa657960ae403d98a356e40687ea0577a
Tomas <maceiratomas@gmail.com>
2023-08-20 22:29:11 +0200

    Added new error and implemented IntoResponse for Error

--- api/src/error.rs
+++ api/src/error.rs
@@ -1,9 +1,27 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
 use serde::Serialize;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Serialize, strum_macros::AsRefStr)]
 #[serde(tag = "type", content = "data")]
 pub enum Error {
-    DBError { data: String },
+    DBError { error: String },
+    WorkerNotFound { id: String },
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        println!("INTO RESPONSE {self:?}");
+
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        // Insert the Error into the reponse.
+        response.extensions_mut().insert(self);
+
+        response
+    }
 }
